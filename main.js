39a9ffa105f8dd01f3728db2a772056ca70daff8
Uchenna <okorocode@gmail.com>
2019-06-25 20:13:01 +0100

    chores: fetched issues for display

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ function fetchIssues() {
     // getting the issues from localStorage
     const issues = JSON.parse(localStorage.getItem('issues')); 
 
-    issuesList.innerHTML = '';
+    issuesList.innerHTML += '';  // clearing the list of issues on load
 
-    issues.array.forEach(element => {
-        
-    });
+    issues.forEach(issue => {
+        const id = issue.id,
+              description = issue.description,
+              severity = issue.severity,
+              assignedTo = issue.assignedTo,
+              status = issue.status;
+
+        issuesList.innerHTML = `
+        <div class="well">
+            <h6> Issue ID: ${id} </h6>
+            <p><span class="label label-info"> ${status}</span></p>
+
+            <h3>${description}</h3>
+            <p>
+                <span class="glyphicon glyphhicon-time"></span>${severity}
+                <span class="glyphicon glyphhicon-time"></span>${assignedTo} 
+            </p>
+
+            <a href="#" class="btn btn-warning" onclick="setStatusClosed(/${id}/)">Close</a>
+            <a href="#" class="btn btn-danger" onclick="deleteIssue(/${id}/)">Delete</a>
+
+        </div>`
+    })
 }

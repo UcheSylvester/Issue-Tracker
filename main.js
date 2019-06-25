ceb6676126ea3203280c9b74c55facbb88c30af2
Uchenna <okorocode@gmail.com>
2019-06-26 00:55:03 +0100

    chores: saving issues done

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+console.log('working...')
+
+const form = document.getElementById('issueInputForm')
+const issuesList = document.getElementById('issuesList');
+
 function fetchIssues() {
-    const issuesList = Document.getElementById('issueList');
-    // getting the issues from localStorage
-    const issues = JSON.parse(localStorage.getItem('issues')); 
+    console.log('fetching issues from localStoage')
+    let issues = JSON.parse(localStorage.getItem('issues'));  // getting the issues from localStorage
 
-    issuesList.innerHTML += '';  // clearing the list of issues on load
-
+    issuesList.innerHTML = '';  // clearing the list of issues on load
+    
     issues.forEach(issue => {
         const id = issue.id,
               description = issue.description,
@@ -12,7 +16,8 @@ function fetchIssues() {
               assignedTo = issue.assignedTo,
               status = issue.status;
 
-        issuesList.innerHTML = `
+        // Displaying issues
+        issuesList.innerHTML += `
         <div class="well">
             <h6> Issue ID: ${id} </h6>
             <p><span class="label label-info"> ${status}</span></p>
@@ -29,3 +34,66 @@ function fetchIssues() {
         </div>`
     })
 }
+
+
+function saveIssue(e) {
+    e.preventDefault()
+
+    console.log('saving issues')  
+    let issueId = chance.guid(),
+        issueDescription = document.getElementById('issueDescInput').value,
+        issueSeverity = document.getElementById('issueSeverityInput').value,
+        issueAssignedTo = document.getElementById('issueAssignedToInput').value,
+        issueStatus = 'open';
+
+    const issue = {
+        id: issueId,
+        description: issueDescription,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    // checking is localStorage exist in browser window
+    if(window.localStorage) {
+        let issues = [];
+
+        if(localStorage.getItem('issues') == 'undefined') {
+
+            issues.push(issue);
+            localStorage.setItem('issues', JSON.stringify(issues));
+            console.log(issues)
+        }
+        else {
+            let issues = JSON.parse(localStorage.getItem('issues'));
+            issues.push(issue);
+            localStorage.setItem('issues', JSON.stringify(issues))
+        }
+    } else {
+        console.warn('localStorage does not exist')
+
+    }
+
+
+
+
+    // if(localStorage.getItem('issues') === 'null') {
+    //     let issues = [];
+    //     issues.push(issue)
+    //     localStorage.setItem('issues', JSON.stringify(issues))
+    // } 
+    // else {
+    //     let issues = JSON.parse(localStorage.getItem('issues'));
+    //     issues.push(issue);
+    //     localStorage.setItem('issues', JSON.stringify(issues))
+    // }
+
+    form.reset();
+
+    fetchIssues();
+    
+}
+
+form.addEventListener('submit', saveIssue)
+
+// onload="fetchIssues()"
